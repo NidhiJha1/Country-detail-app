88e20faa4b2d7b17deac868cb5c31e3479027acd
Nidhi kumari <ssc.nidhi@gmail.com>
2021-09-11 13:29:18 +0530

    my second commit

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -15,32 +15,7 @@ const CountryList = ({country,searchName}) =>{
                      <th> Border</th>
                      <th>Languages</th>
     </tr>
-       {/* {
-         country.map((curElem,index) => (
-           
-                <tr key={index}>
-                  <td>
-                    {curElem.name}
-                  </td>
-                  <td>
-                    <img src={curElem.flag} alt={curElem.name}/>
-                  </td>
-                  <td>
-                    {curElem.nativeName}
-                  </td>
-                  <td>
-                    { curElem.borders.toString() }
-                  </td>
-                  <td>
-                    <div className="language-name">
-                    {curElem.languages.map((data,index) =>(
-                       <div key={index}>{data.name},</div>
-                    ))} 
-                  </div>
-                  </td>
-                </tr> 
-         ))
-       } */}
+       
 
 {
          country.filter((val) =>{
